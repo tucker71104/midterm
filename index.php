6e372972dfa686466997037e2f46df8bbccc2940
tucker71104 <danko_tiffany@columbusstate.edu>
2017-10-05 16:06:37 +0000

    Second Commit

--- index.php
+++ index.php
@@ -3,21 +3,30 @@
     
     //Set search term or hard-code the parameter value
     $state = CA;
+    $version = 2.0;
+    $customerID = 1004;
     
-    $query = "SELECT firstName, lastName, city FROM customers WHERE state = ? ORDER BY lastName";
+    //$query = "SELECT firstName, lastName, city, productCode FROM customers, products WHERE state = ? and version = ? ORDER BY lastName";
+    //$query = "SELECT firstName, lastName, city, productCode, name FROM customers, products WHERE state = ? and customerID = ? ORDER BY lastName";
+    $query = "SELECT firstName, lastName, productCode, name FROM customers, products WHERE customerID = ? ORDER BY lastName";
+
     $stmt = $db->prepare($query);
-    $stmt->bind_param('s', $state);
+    //$stmt->bind_param('ss', $state, $version);
+    $stmt->bind_param('s', $customerID);
     $stmt->execute();
     
     $stmt->store_result();
     //store result fields in variables
-    $stmt->bind_result($firstName, $lastName, $city);
+    //$stmt->bind_result($firstName, $lastName, $city, $productCode, $name);
+    $stmt->bind_result($firstName, $lastName, $productCode, $name);
  
     //use this method to display results
     while($stmt->fetch()) {
       echo "<p>First Name: ".$firstName;
       echo "<br />Last Name: ".$lastName;
-      echo "<br />City: ".$city;
+      //echo "<br />City: ".$city;
+      echo "<br />Product Code: ".$productCode;
+      echo "<br />Name: ".$name;
     }
     
     $stmt->free_result();
@@ -50,13 +59,15 @@
                 <tr>
                     <th>First Name</th>
                     <th>Last Name</th>
-                    <th>City</th>
+                    <th>Product Code</th>
+                    <th>Product Name</th>
                 </tr>
                 <tr>
                 <?php while($stmt->fetch()) ?> 
                         <td><?php echo $customer.$firstName; ?></td>
                         <td><?php echo $customer.$lastName; ?></td>
-                        <td><?php echo $customer.$city; ?></td>
+                        <td><?php echo $customer.$productCode; ?></td>
+                        <td><?php echo $customer.$name; ?></td>
                 </tr>
             </table>
 
